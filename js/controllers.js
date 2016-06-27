d5286c74669ae810cbea7c2131a37c4e7e7568d1
Tristan Wells <tristan.wells@achieveinternet.com>
2016-06-26 20:27:01 -0700

    exmaple2

--- js/controllers.js
+++ js/controllers.js
@@ -7,23 +7,7 @@ var lunchAndLearnExample = angular.module('lunchAndLearnExample', []);
 
 lunchAndLearnExample.controller('StockDataController', function ($scope,$http) {
   $scope.dataBindingExample= 'Hello data binding example one';
-  $scope.isTodayTuesday = true;
-  //Http request
-  $scope.getStockData = function (){
-    $http.get("mockphp/getTable.php").then(function(response){
-      $scope.databaseresponse=response.data;
-      console.log($scope.databaseresponse);
-    });
-  };
-
-  $scope.toggleBoolean = function(){
-    if($scope.isTodayTuesday === true){
-      $scope.isTodayTuesday = false;
-      return;
-    }
-    $scope.isTodayTuesday= true;
-  }
-
+ 
 
 
 
